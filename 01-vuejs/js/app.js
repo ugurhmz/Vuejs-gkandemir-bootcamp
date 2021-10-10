3629bb8ac36980bc22c36664337dac9e714af0ea
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-10-10 15:53:35 +0300

    Event binding, Attribute bindind

--- 01-vuejs/js/app.js
+++ 01-vuejs/js/app.js
@@ -5,13 +5,18 @@ const app = Vue.createApp({
     // DATA
         data() {
             return {
-                title:"HOş geldiniz",
+                title:"Hoş geldiniz",
                 description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias beatae deserunt facilis fugiat illo laudantium libero magni minus molestiae nam nobis optio, quia quibusdam reiciendis, sit suscipit veniam voluptatem!",
                 eduflow: {
                     title:"Google Gider",
                     target:"_blank",
                     url:"http://www.google.com",
                     alt:"google-git"
+                },
+
+                mycoords : {
+                    x:0,
+                    y:0
                 }
             };
         },
@@ -20,6 +25,16 @@ const app = Vue.createApp({
     methods : {
             changeTitle(mytitle) {
                 this.title = mytitle;
+            },
+
+            updateCoords(event, message){
+               this.changeTitle(`${event.x}, ${event.y}`)
+
+              this.mycoords = {
+                  x : event.x,
+                  y : event.y
+                }
+
             }
     }
 
---
+++ 02-events/app.js
@@ -0,0 +1,18 @@
+
+
+const app = Vue.createApp({
+
+    data () {
+        return {
+            fullName:null,
+        }
+    } ,
+
+    methods : {
+        updateValue(event) {
+            this.fullName = event.target.value;
+        }
+    }
+
+
+}).mount("#app");
