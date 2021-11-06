2f54c56292a057c5e55a7af5960f6408e4cba73e
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-11-06 16:47:12 +0300

    PersistedState usage, secure-ls with LocalStorage

--- 20-app-socialmark/src/store/index.js
+++ 20-app-socialmark/src/store/index.js
@@ -1,4 +1,8 @@
 import {createStore} from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
+
 
 
 export default createStore({
@@ -21,7 +25,17 @@ export default createStore({
             return myuser
         },
         _mykey : state => state.mykey   // bütün her yerden kullan diye bu şekilde yaptık.
-    }
+    },
+
+    plugins: [
+        createPersistedState({
+            storage: {
+                getItem: (key) => ls.get(key),
+                setItem: (key, value) => ls.set(key, value),
+                removeItem: (key) => ls.remove(key),
+            },
+        }),
+    ],
 
 })
 
