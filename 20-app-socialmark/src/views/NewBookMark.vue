cec9f9637d73761adf5929cf960b4e690ec6a4c8
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-11-08 03:06:25 +0300

    NewBookMark post, currentuser

--- 20-app-socialmark/src/views/NewBookMark.vue
+++ 20-app-socialmark/src/views/NewBookMark.vue
@@ -2,26 +2,71 @@
 
   <div class="login_register_container ">
     <h3 class="text-2xl text-center mb-3">Yeni Ekle</h3>
-    <input type="text" placeholder="Başlık" class="input mb-3" />
-    <input type="text" placeholder="URL" class="input mb-3" />
-    <select class="input mb-3">
+
+    <input v-model="userData.title" type="text" placeholder="Başlık" class="input mb-3" />
+    <input v-model="userData.url" type="text" placeholder="URL" class="input mb-3" />
+
+    <select v-model="userData.category_id"  class="input mb-3">
       <option disabled value="" selected>Kategori</option>
-      <option value="">Vue</option>
-      <option value="">React</option>
-      <option value="">Sosyal</option>
+      <option v-for="category in categoryList" :key="category.id" :value="category.id">
+            {{ category.name }}
+      </option>
+
     </select>
-    <textarea placeholder="Açıklama" class="input mb-3" cols="30" rows="10"></textarea>
+
+    <textarea v-model="userData.description"  placeholder="Açıklama" class="input mb-3" cols="30" rows="10"></textarea>
     <div class="flex items-center justify-end gap-x-1">
-      <button class="secondary-button">İptal</button>
-      <button class="default-button">Kaydet</button>
+      <button @click="$router.push({ name : 'HomePage' })" class="secondary-button">İptal</button>
+      <button @click="onSave" class="default-button">Kaydet</button>
     </div>
   </div>
 
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+
 export default {
-  name: "NewBookMark"
+  data() {
+    return {
+        categoryList : [],
+        userData : {
+            title: null,
+            url : null,
+            category_id : null,
+            description : null
+        }
+    }
+  },
+
+  mounted() {
+    this.$appAxios.get("/categories").then(categories_response => {
+      this.categoryList = categories_response?.data || [];
+    })
+
+  },
+
+  methods : {
+    onSave() {
+
+      const saveData = {
+        ... this.userData,
+        user_id : this._getCurrentUser?.id,   // bu save işlemini yapan kullanıcının idsi.
+        created_at : new Date() // save dediği anda tarih create olsun.
+      };
+
+      // /bookmarks'a post et, saveData objesiyle beraber post et.
+      this.$appAxios.post("/bookmarks",saveData).then(save_bookmark_response => {
+        console.log(save_bookmark_response)
+      })
+
+    }
+  },
+
+  computed : {
+    ... mapGetters(["_getCurrentUser"])
+  }
+
 }
 </script>
 
